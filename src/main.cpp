0e0c81b57263ae830b6629d92440d3cd0fff8cd6
dimkatet <tet.dima.211@gmail.com>
2018-05-25 02:47:50 +0700

    Edit sdafx.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
- int main()
- {
- 	
- }
+#include "sdafx.hpp"
+#include "write.hpp"
+int main()
+{
+	Note *NotesDay = new Note [10]; 
+	int nowDay, nowMonth, year = 2018;
+	FILE *data;
+ 	Printf(data,NotesDay,nowMonth,year);
+ 	return 0;
+}
