b29338f3ccae42652025dac04291ce4ad12f4cc7
dimkatet <tet.dima.211@gmail.com>
2018-05-25 14:25:27 +0700

    Edit sdafx.hpp

--- src/sdafx.hpp
+++ src/sdafx.hpp
@@ -4,14 +4,6 @@
 #include <ctime>
 #include <cstdlib>
 #include <cstdio>
-#include <SFML/Graphics.hpp>
-#include <tchar.h> 
-#include <sstream> 
-#include <conio.h> 
-#include <string.h>
-#include <windows.h>
-#include "read.hpp"
-#include ""
 struct text{
 	char t_text[60];
 	int t_metadata;
