38218fa9f32170a06c80eeed51874f0bc750e88e
dimkatet <tet.dima.211@gmail.com>
2018-05-25 21:46:56 +0700

    Add edit.cpp with Clean

---
+++ src/edit.cpp
@@ -0,0 +1,18 @@
+#include "sdafx.hpp"
+#include "edit.hpp"
+void Clean(Note *Notes,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Notes[i].n_data.tm_hour = 0;
+		Notes[i].n_data.tm_min = 0;
+		Notes[i].n_data.tm_mon = 0;
+		Notes[i].n_data.tm_year = 0;
+		Notes[i].n_data.tm_mday = 0;
+		for (int j = 0; j < 10; j++)
+		{
+			Notes[i].n_text[j].t_metadata = 0;
+			Notes[i].n_text[j].t_text[0] = '\0';
+		}
+	}
+}
