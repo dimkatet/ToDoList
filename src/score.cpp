8279b54b0fa6d2d5258f7135caaea24d4142c3e7
dimkatet <tet.dima.211@gmail.com>
2018-05-25 22:43:53 +0700

    add score.cpp with ScoreTask and ScoreTaskMonth

---
+++ src/score.cpp
@@ -0,0 +1,22 @@
+#include <sdafx.hpp>
+#include <score.hpp>
+void ScoreTask(Note *Notes, Note *NotesDay, int *num, int data)
+{
+	int i = 0;
+	*num = 0;
+	while (Notes[i].n_data.tm_mday != data && Notes[i].n_text[0].t_metadata!=0)
+		i++;
+
+	while (Notes[i].n_data.tm_mday == data) {
+		NotesDay[*num] = Notes[i];
+		*num=*num+1;
+		i++;
+	}
+}
+
+void ScoreTaskMonth(Note *Notes, int *num)
+{
+	*num = 0;
+	while(Notes[*num].n_text[0].t_metadata!=0)
+		*num=*num+1;
+}
